dbe92a1c80dcaf0ea87b6fcda3758350fd596a6e
Fadliabdill <fadliabdill287@gmail.com>
2024-12-15 01:25:39 +0700

    first commit

---
+++ lib/config/api.dart
@@ -0,0 +1,7 @@
+class Api {
+  // static const baseUrl = 'https://moneyrecord.fajarms.com';
+  static const baseUrl = 'https://7c23-36-69-143-50.ngrok-free.app/api';
+
+  static const absen = '$baseUrl/absensi';
+  static const user = '$baseUrl/user';
+}
